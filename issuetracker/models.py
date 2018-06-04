5cfeaa454b25b9081b890b005f14a72affcf847b
Raymond Rupiah <rupiray@yahoo.com>
2018-06-04 02:09:16 +0100

    defined models and migrations

--- issuetracker/models.py
+++ issuetracker/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS_CHOICES = (
+    (1, 'Open'),
+    (2, 'Resolved'),
+    (3, 'Closed'),
+)
+
+PRIORITY_CHOICES = (
+    (1, 'Critical'),
+    (2, 'High'),
+    (3, 'Low'),
+)
+
+# category
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural= 'categories'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+# issue
+class Issue(models.Model):
+    title = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    submitted_date = models.DateField(auto_now_add=True)
+    submitter = models.ForeignKey(User, related_name="submitter", on_delete=models.PROTECT)
+    solver = models.ForeignKey(User, on_delete=models.PROTECT)
+    description = models.TextField(blank=True)
+    priority = models.IntegerField(default=1, choices=PRIORITY_CHOICES)
+    status = models.IntegerField(default=1, choices=STATUS_CHOICES)
+
+    class Meta:
+        verbose_name = 'issue'
+        verbose_name_plural = 'issues'
+        ordering = ('status', 'submitted_date', 'title')
+
+    def __str__(self):
+        return self.title
