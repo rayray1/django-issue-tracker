ed28c45eaddfefed361b128d599051212fe30e3c
Raymond Rupiah <rupiray@yahoo.com>
2018-06-04 02:29:50 +0100

    modified admin, added staff user role

--- issuetracker/admin.py
+++ issuetracker/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from issuetracker.models import Issue, Category
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(Category, CategoryAdmin)
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'priority', 'submitter', 'submitted_date')
+    list_filter = ('status', 'submitted_date')
+    search_fields = ('title', 'description',)
+
+admin.site.register(Issue, IssueAdmin)
